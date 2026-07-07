a431bd14769b4366d1f9f7cfc06456a1afaff207
assmaabaaj <elsabaaj@gmail.com>
2026-07-07 19:07:12 +0300

    Create P4.cpp

---
+++ P4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>  
+#include <string>   
+
+using namespace std; 
+
+struct  stInfo
+{
+  int Age;
+  bool HasDrivingLicense;
+};
+
+stInfo ReadInfo()
+{
+  stInfo Info;
+  cout << "Please enter your age: " << endl;
+  cin >> Info.Agr;
+  cout << "Do you have a driver’s license? (1 for Yes, 0 for No)" << endl;
+  cin >> Info.HasDrivingLicense;
+
+    return Info;
+}
+
+bool IsAccepted(stInfo Info)
+{
+  return (Info.Age > 21 && Info.HasDrivingLicense)
+}
+
+void PrintResult(stInfo Info)
+{
+  if(IsAccepted(Info))
+    cout << "\n Hired" << endl;
+  else
+     cout << "\n Rejected" << endl;
+}
+
+
+int main()
+{
+   PrintResult(ReadInfo());
+    return 0; 
+}
