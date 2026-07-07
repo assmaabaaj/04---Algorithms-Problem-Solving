c901ed2e688592eb6bfadcdbcfb24f945dbca327
assmaabaaj <elsabaaj@gmail.com>
2026-07-07 18:42:52 +0300

    Create P1.cpp

---
+++ P1.cpp
@@ -0,0 +1,15 @@
+include <iostream>
+include <string>
+
+using namespace std;
+
+void PrintName(string Name)
+{
+  cout << "\n Your Name Is: " << Name << endl;
+}
+
+int main()
+{
+  PrintName("Assmaa");
+  return 0;
+}
